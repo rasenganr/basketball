229038275ccf98399f0d2715c31b79d10a45c59c
hw243 <hw243@duke.edu>
2020-03-08 18:48:35 -0400

    Four "add" functions finished

--- exerciser.cpp
+++ exerciser.cpp
@@ -1,4 +1,11 @@
 #include "exerciser.h"
 
 void exercise(connection * C) {
+  add_state(C, "NC");
+  add_state(C, "VA");
+  add_color(C, "LightBlue");
+  add_color(C, "Orange");
+  add_player(C, 1, 1, "Jerome", "Robinson", 34, 19, 4, 3, 1.7, 0.4);
+  add_player(C, 1, 0, "Ky", "Bowman", 29, 14, 5, 3, 1.2, 0.1);
+  add_team(C, "BostonCollege", 10, 7, 2, 16);
 }
--- query_funcs.cpp
+++ query_funcs.cpp
@@ -1,17 +1,31 @@
 #include "query_funcs.h"
 
+// -------------------------------------------
+// Drop previous relations and create new ones
+// -------------------------------------------
 void setupTables(connection * C) {
-  string sql_create_player =
-      "DROP TABLE IF EXISTS PLAYER;"
+  string sql =
+      "DROP TABLE IF EXISTS PLAYER CASCADE;"
       "CREATE TABLE PLAYER (PLAYER_ID INT NOT NULL, TEAM_ID INT NOT NULL, "
       "UNIFORM_NUM INT NOT NULL, FIRST_NAME VARCHAR(15) NOT NULL, LAST_NAME VARCHAR(15) "
       "NOT NULL, MPG "
-      "FLOAT, PPG FLOAT, RPG FLOAT, APG FLOAT, SPG FLOAT, BPG FLOAT, PRIMARY "
-      "KEY(PLAYER_ID));";
+      "INT, PPG INT, RPG INT, APG INT, SPG FLOAT, BPG FLOAT, PRIMARY "
+      "KEY(PLAYER_ID), FOREIGN KEY(TEAM_ID) REFERENCES TEAM(TEAM_ID) ON DELETE CASCADE);"
+      "DROP TABLE IF EXISTS TEAM CASCADE; CREATE TABLE TEAM (TEAM_ID INT NOT NULL, NAME "
+      "VARCHAR(20) NOT NULL, STATE_ID INT NOT NULL, COLOR_ID INT NOT NULL, WINS INT NOT "
+      "NULL, LOSSES INT NOT NULL, PRIMARY KEY(TEAM_ID), FOREIGN KEY(STATE_ID) REFERENCES "
+      "STATE(STATE_ID) ON DELETE CASCADE, FOREIGN KEY(COLOR_ID) REFERENCES "
+      "COLOR(COLOR_ID) ON DELETE CASCADE);"
+      "DROP TABLE IF EXISTS STATE CASCADE;"
+      "CREATE TABLE STATE (STATE_ID INT NOT NULL, NAME VARCHAR(20) NOT NULL, PRIMARY "
+      "KEY(STATE_ID));"
+      "DROP TABLE IF EXISTS COLOR CASCADE;"
+      "CREATE TABLE COLOR (COLOR_ID INT NOT NULL, NAME VARCHAR(20) NOT NULL, PRIMARY KEY "
+      "(COLOR_ID));";
   work W(*C);
-  W.exec(sql_create_player);
+  W.exec(sql);
   W.commit();
-  cout << "PLAYER created" << endl;
+  cout << "Relations created" << endl;
 }
 
 void add_player(connection * C,
@@ -25,6 +39,18 @@ void add_player(connection * C,
                 int apg,
                 double spg,
                 double bpg) {
+  static int player_id = 1;
+  string sql = "INSERT INTO PLAYER VALUES (" + to_string(player_id) + ", " +
+               to_string(team_id) + ", " + to_string(jersey_num) + ", " + "\'" +
+               first_name + "\', \'" + last_name + "\', " + to_string(mpg) + ", " +
+               to_string(ppg) + ", " + to_string(rpg) + ", " + to_string(apg) + ", " +
+               to_string(spg) + ", " + to_string(bpg) + ");";
+  work W(*C);
+  W.exec(sql);
+  W.commit();
+  cout << "Player " << player_id << "(" << first_name << " " << last_name << ") added"
+       << endl;
+  ++player_id;
 }
 
 void add_team(connection * C,
@@ -33,12 +59,37 @@ void add_team(connection * C,
               int color_id,
               int wins,
               int losses) {
+  static int team_id = 1;
+  string sql = "INSERT INTO TEAM VALUES (" + to_string(team_id) + ", \'" + name + "\', " +
+               to_string(state_id) + ", " + to_string(color_id) + ", " + to_string(wins) +
+               ", " + to_string(losses) + ");";
+  work W(*C);
+  W.exec(sql);
+  W.commit();
+  cout << "Team " << team_id << "(" << name << ") added" << endl;
+  ++team_id;
 }
 
 void add_state(connection * C, string name) {
+  static int state_id = 1;
+  string sql =
+      "INSERT INTO STATE VALUES (" + to_string(state_id) + ", \'" + name + "\');";
+  work W(*C);
+  W.exec(sql);
+  W.commit();
+  cout << "State " << state_id << "(" << name << ") added" << endl;
+  ++state_id;
 }
 
 void add_color(connection * C, string name) {
+  static int color_id = 1;
+  string sql =
+      "INSERT INTO COLOR VALUES (" + to_string(color_id) + ", \'" + name + "\');";
+  work W(*C);
+  W.exec(sql);
+  W.commit();
+  cout << "Color " << color_id << "(" << name << ") added" << endl;
+  ++color_id;
 }
 
 void query1(connection * C,
